c373e24abb75df041da81e0e3460e0414dc55be1
fenyb_ <jetsribumrungtechin@gmail.com>
2025-10-19 04:02:39 +0700

    Update register_screen.dart

--- homeservice-frontend/lib/screens/register_screen.dart
+++ homeservice-frontend/lib/screens/register_screen.dart
@@ -35,7 +35,7 @@ class _RegisterState extends ConsumerState<RegisterScreen> {
   static const _card = Colors.white;
   static const _textMain = Color(0xFF0B1220); 
   static const _textSub = Color(0xFF475569);
-  static const _divider = Color(0xFFE5E7EB); // gray-200
+  static const _divider = Color(0xFFE5E7EB); 
   static const _fieldFill = Color(0xFFFFFFFF); // inputs white
 
   @override
