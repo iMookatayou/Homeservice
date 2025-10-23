1e8ef023dde007c8d0ca2fab3ebeab1c784121f8
fenyb_ <jetsribumrungtechin@gmail.com>
2025-10-24 00:47:02 +0700

    Update handler.go

--- homeservice-backend/internal/files/handler.go
+++ homeservice-backend/internal/files/handler.go
@@ -21,7 +21,6 @@ type Handler struct {
 }
 
 func (h Handler) RegisterRoutes(r chi.Router) {
-	// local upload
 	r.Post("/uploads", h.UploadLocal)
 	// s3 presign (ยังไม่รองรับใน LocalStorage -> จะส่ง 501)
 	r.Post("/uploads/presign", h.Presign)
